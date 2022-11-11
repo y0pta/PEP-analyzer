c166b7818986d87c599b6127ddedf1a89fd984b1
Yopta <lizavetka-sim@yandex.ru>
2022-11-11 10:02:38 +0400

    Add more PEP rules, amend PEPChecker logic.

--- code_analyzer.py
+++ code_analyzer.py
@@ -1,4 +1,5 @@
 import sys, os
+import re
 
 output = "{file}: Line {line}: {code} {message}"
 
@@ -10,6 +11,12 @@ class PEPError:
     def check(self):
         pass
 
+    @staticmethod
+    def find_construction(line, construction):
+        pos_start = line.find(construction)
+        pos_end = pos_start + len(construction)
+        return None, None if pos_start == -1 else pos_start, pos_end
+
 
 class LineLenError(PEPError):
     code = "S001"
@@ -17,7 +24,7 @@ class LineLenError(PEPError):
 
     @staticmethod
     def check(line):
-        return len(line) > 79
+        return LineLenError() if len(line) > 79 else None
 
 
 class IndentError(PEPError):
@@ -32,7 +39,7 @@ class IndentError(PEPError):
                 num_spaces += 1
             else:
                 num_spaces -= 1
-                return num_spaces % 4
+                return IndentError() if num_spaces % 4 else None
 
 
 class SemicolonError(PEPError):
@@ -47,7 +54,7 @@ class SemicolonError(PEPError):
         l = l.replace(" ", "")
         l = l.replace('\n', '')
         last_symb = l[-1] if len(l) > 0 else ''
-        return last_symb == ';'
+        return SemicolonError() if last_symb == ';' else None
 
 
 class SpaceCommentsError(PEPError):
@@ -59,9 +66,9 @@ class SpaceCommentsError(PEPError):
         line_strip = line.strip()
         comment_pos = line_strip.find('#')
         if comment_pos < 1:  # -1, 0
-            return False
+            return None
         right_comment_pos = line.find('  #', 0, comment_pos + 1)
-        return right_comment_pos == -1
+        return SpaceCommentsError if right_comment_pos == -1 else None
 
 
 class TODOError(PEPError):
@@ -72,7 +79,7 @@ class TODOError(PEPError):
     def check(line):
         line = line.lower()
         todo_pos = line.find('todo')
-        return -1 < line.find('#') < todo_pos
+        return TODOError() if -1 < line.find('#') < todo_pos else None
 
 
 class BlankLinesError(PEPError):
@@ -84,18 +91,81 @@ class BlankLinesError(PEPError):
         res = []
         num_blank = 0
         for i in range(0, len(text_lines)):
-            line = text_lines[i].replace('\n', '')
-            if len(line) == 0:
+            line = text_lines[i]
+            if line[0] == '\n':
                 num_blank += 1
             else:
                 if num_blank > 2:
                     res.append(i)
-                    num_blank = 0
+                num_blank = 0
         return res
 
 
+class SpaceConstructionError(PEPError):
+    code = "S006"
+    message = "Too many spaces after construction_name \'{construction}\'."
+
+    def __init__(self, construction):
+        self.message = SpaceConstructionError.message.format(construction=construction)
+
+    @staticmethod
+    def check(line):
+        class_re = 'class\s\s+'
+        result = SpaceConstructionError('class') if re.match(class_re, line) else None
+
+        def_re = 'def\s\s+'
+        return SpaceConstructionError('def') if re.match(def_re, line) else result
+
+
+class ClassNameError(PEPError):
+    code = "S007"
+    message = "Class name \'{class_name}\' should be written in CamelCase."
+
+    def __init__(self, class_name):
+        self.message = ClassNameError.format(class_name=class_name)
+
+    @staticmethod
+    def check(line):
+        pos = line.find('class ')
+        if pos > -1:
+            pos += len('class ')
+            pos_end = line.find('(')
+            name = line[pos:pos_end]
+            regexp = "[A-Z][^_]*"
+            if not re.match(regexp, name):
+                return ClassNameError(name)
+        return None
+
+
+class FunctionNameError(PEPError):
+    code = "S008"
+    message = "Function name \'{function_name}\' should be written in snake_case."
+
+    def __init__(self, func_name):
+        self.message = FunctionNameError.message.format(function_name=func_name)
+
+    @staticmethod
+    def check(line):
+        pos = line.find('def ')
+        if pos > -1:
+            pos += len('def ')
+            pos_end = line.find('(')
+            name = line[pos:pos_end]
+            regexp = "[_a-z0-9]+"
+            if not re.match(regexp, name):
+                return FunctionNameError(name)
+        return None
+
+
 class PEPChecker:
-    single_line_errors = [LineLenError, IndentError, SemicolonError, SpaceCommentsError, TODOError]
+    single_line_errors = [LineLenError,
+                          IndentError,
+                          SemicolonError,
+                          SpaceCommentsError,
+                          TODOError,
+                          ClassNameError,
+                          SpaceConstructionError,
+                          FunctionNameError]
     multi_line_errors = [BlankLinesError]
     errors = single_line_errors + multi_line_errors
 
@@ -103,6 +173,7 @@ class PEPChecker:
         self.filename = fname
         self.lines = []
         self.error_dict = {}
+        self.lines_errors = []
         with open(fname, 'r') as f:
             self.lines = f.readlines()
         self._check()
@@ -127,19 +198,18 @@ class PEPChecker:
         raise StopIteration
 
     def _check(self):
+        self.lines_errors = [[] for x in range(len(self.lines))]
+        # check single lines errors
         for i, line in enumerate(self.lines):
-            for error in self.errors:
-                self.error_dict[error] = self.check_error(error)
+            for error in PEPChecker.single_line_errors:
+                self.lines_errors[i] = error.check(line)
 
-    def check_error(self, error: PEPError) -> list:
-        if isinstance(error(), tuple(PEPChecker.multi_line_errors)):
-            return error.check(self.lines)
+        # looking for multiline errors
+        for error in PEPChecker.multi_line_errors:
+            lines_with_err = error.check(self.lines)
+            for line in lines_with_err:
+                self.lines_errors[line].append(error())
 
-        error_line_nums = []
-        for i, line in enumerate(self.lines):
-            if error.check(line):
-                error_line_nums.append(i)
-        return error_line_nums
 
     @staticmethod
     def check_line(line, error):
@@ -153,10 +223,11 @@ files = []
 if os.path.isfile(arg):
     files.append(arg)
 else:
-    files = [os.path.join(arg, file) for file in os.listdir(arg) if os.path.basename(file).endswith('.py') and "tests.py" not in file]
+    files = [os.path.join(arg, file) for file in os.listdir(arg) if
+             os.path.basename(file).endswith('.py') and "tests.py" not in file]
 
 for file in files:
     checker = PEPChecker(file)
     for line_num, errors in enumerate(checker):
         for err in errors:
-            print(output.format(file=file, line=line_num+1, code=err.code, message=err.message))
+            print(output.format(file=file, line=line_num + 1, code=err.code, message=err.message))
