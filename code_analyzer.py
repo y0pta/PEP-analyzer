d644f9dacc9917ddbdff171b1299a7ef026d4a35
Yopta <lizavetka-sim@yandex.ru>
2022-11-11 18:05:29 +0400

    Add new checkpoints for variable names

--- code_analyzer.py
+++ code_analyzer.py
@@ -1,5 +1,5 @@
 import sys, os
-import re
+import re, ast
 
 output = "{file}: Line {line}: {code} {message}"
 
@@ -88,7 +88,7 @@ class BlankLinesError(PEPError):
 
     @staticmethod
     def check(text_lines):
-        res = []
+        res = [None for i in range(len(text_lines))]
         num_blank = 0
         for i in range(0, len(text_lines)):
             line = text_lines[i]
@@ -96,13 +96,13 @@ class BlankLinesError(PEPError):
                 num_blank += 1
             else:
                 if num_blank > 2:
-                    res.append(i)
+                    res[i] = BlankLinesError()
                 num_blank = 0
         return res
 
 
 class SpaceConstructionError(PEPError):
-    code = "S006"
+    code = "S007"
     message = "Too many spaces after construction_name \'{construction}\'."
 
     def __init__(self, construction):
@@ -110,51 +110,177 @@ class SpaceConstructionError(PEPError):
 
     @staticmethod
     def check(line):
-        class_re = 'class\s\s+'
+        class_re = '\s*class \s+'
         result = SpaceConstructionError('class') if re.match(class_re, line) else None
 
-        def_re = 'def\s\s+'
-        return SpaceConstructionError('def') if re.match(def_re, line) else result
+        def_re = '\s*def \s+'
+        result = SpaceConstructionError('def') if re.match(def_re, line) else result
+        return result
 
 
 class ClassNameError(PEPError):
-    code = "S007"
+    code = "S008"
     message = "Class name \'{class_name}\' should be written in CamelCase."
 
     def __init__(self, class_name):
-        self.message = ClassNameError.format(class_name=class_name)
+        self.message = ClassNameError.message.format(class_name=class_name)
 
     @staticmethod
     def check(line):
         pos = line.find('class ')
         if pos > -1:
             pos += len('class ')
-            pos_end = line.find('(')
-            name = line[pos:pos_end]
+            name = re.match('\s*\S*?[:(]', line[pos:]).group()[:-1]
+            name = name.strip()
+
             regexp = "[A-Z][^_]*"
             if not re.match(regexp, name):
                 return ClassNameError(name)
         return None
 
 
-class FunctionNameError(PEPError):
-    code = "S008"
-    message = "Function name \'{function_name}\' should be written in snake_case."
+class AstHelper:
+    class Function:
+        def __init__(self, name: str, lineno: int, args: list):
+            self.name = name
+            self.lineno = lineno
+            self.args = args
 
-    def __init__(self, func_name):
-        self.message = FunctionNameError.message.format(function_name=func_name)
+    def __init__(self, text):
+        self.text = text
+        self.ast_tree = ast.parse(text)
+        self.nodes = ast.walk(self.ast_tree)
+
+    def all_functions(self):
+        res = []
+        for node in self.nodes:
+            if isinstance(node, ast.FunctionDef):
+                res.append(node)
+        return res
 
     @staticmethod
-    def check(line):
-        pos = line.find('def ')
-        if pos > -1:
-            pos += len('def ')
-            pos_end = line.find('(')
-            name = line[pos:pos_end]
-            regexp = "[_a-z0-9]+"
-            if not re.match(regexp, name):
-                return FunctionNameError(name)
-        return None
+    def get_function_args(ast_func: ast.FunctionDef) -> dict:
+        res = {}
+
+        args = ast_func.args.args
+        default_args = ast_func.args.defaults
+        for i, arg in enumerate(args):
+            # find default if exists
+            default_value = None
+            def_i = len(default_args) - len(args) + i
+            if def_i >= 0:
+                default_value = default_args[def_i]
+            res[arg] = default_value
+
+        return res
+
+    @staticmethod
+    def get_function_vars(ast_func: ast.FunctionDef) -> list:
+        res = []
+
+        assignments = [x for x in ast_func.body if isinstance(x, ast.Assign)]
+
+        for i, ass in enumerate(assignments):
+            for var in ass.targets:
+                res.append(var)
+        return res
+
+
+    @staticmethod
+    def is_mutable(ast_obj):
+        return isinstance(ast_obj, ast.List) or isinstance(ast_obj, ast.Dict) or isinstance(ast_obj, ast.Set)
+
+
+class SnakeCaseFuncError(PEPError):
+    code = "S009"
+    message = "Function name \'{name}\' should be written in snake_case."
+
+    def __init__(self, name):
+        self.code = SnakeCaseFuncError.code
+        self.message = SnakeCaseFuncError.message.format(name=name)
+
+    @staticmethod
+    def check(lines):
+        regexp = "[_a-z0-9]+"
+        ast_helper = AstHelper(''.join(lines))
+
+        res = [None for i in range(len(lines))]
+        for func in ast_helper.all_functions():
+                func_name = func.name
+
+                # function name
+                if not re.match(regexp, func_name):
+                    lineno = func.lineno - 1
+                    res[lineno] = SnakeCaseFuncError(func_name)
+        return res
+
+
+class SnakeCaseArgError(PEPError):
+    code = "S010"
+    message = "Argument name \'{name}\' should be written in snake_case."
+
+    def __init__(self, name):
+        self.code = SnakeCaseArgError.code
+        self.message = SnakeCaseArgError.message.format(name=name)
+
+    @staticmethod
+    def check(lines):
+        regexp = "[_a-z0-9]+"
+        ast_helper = AstHelper(''.join(lines))
+
+        res = [None for i in range(len(lines))]
+        for func in ast_helper.all_functions():
+                args = ast_helper.get_function_args(func)
+
+                # args
+                for arg in args:
+                    if not re.match(regexp, arg.arg):
+                        lineno = arg.lineno - 1
+                        res[lineno] = SnakeCaseArgError(arg.arg)
+
+        return res
+
+
+class SnakeCaseVarError(PEPError):
+    code = "S011"
+    message = "Variable \'{name}\' should be written in snake_case."
+
+    def __init__(self, name):
+        self.code = SnakeCaseVarError.code
+        self.message = SnakeCaseVarError.message.format(name=name)
+
+    @staticmethod
+    def check(lines):
+        regexp = "[_a-z0-9]+"
+        ast_helper = AstHelper(''.join(lines))
+
+        res = [None for i in range(len(lines))]
+        for func in ast_helper.all_functions():
+                vars = ast_helper.get_function_vars(func)
+
+                # vars
+                for var in vars:
+                    if isinstance(var, ast.Name) and not re.match(regexp, var.id):
+                        lineno = var.lineno - 1
+                        res[lineno] = SnakeCaseVarError(var.id)
+        return res
+
+
+class MutableArgError(PEPError):
+    code = "S012"
+    message = "The default argument value is mutable."
+    # mutable data types in Python: list, dictionary, set
+
+    @staticmethod
+    def check(lines):
+        ast_helper = AstHelper(''.join(lines))
+
+        res = [None for i in range(len(lines))]
+        for func in ast_helper.all_functions():
+            for arg, default in ast_helper.get_function_args(func).items():
+                if ast_helper.is_mutable(default):
+                    res[func.lineno - 1] = MutableArgError()
+        return res
 
 
 class PEPChecker:
@@ -164,15 +290,13 @@ class PEPChecker:
                           SpaceCommentsError,
                           TODOError,
                           ClassNameError,
-                          SpaceConstructionError,
-                          FunctionNameError]
-    multi_line_errors = [BlankLinesError]
+                          SpaceConstructionError]
+    multi_line_errors = [BlankLinesError, SnakeCaseArgError, SnakeCaseFuncError, SnakeCaseVarError, MutableArgError]
     errors = single_line_errors + multi_line_errors
 
     def __init__(self, fname):
         self.filename = fname
         self.lines = []
-        self.error_dict = {}
         self.lines_errors = []
         with open(fname, 'r') as f:
             self.lines = f.readlines()
@@ -180,18 +304,14 @@ class PEPChecker:
 
     def __getitem__(self, line_num) -> list:
         """Return all errors in given line number """
-        result = []
-        for error, pages in self.error_dict.items():
-            if line_num in pages:
-                result.append(error)
-        return result
+        return self.lines_errors[line_num]
 
     def __iter__(self):
         self.current_index = 0
         return self
 
     def __next__(self):
-        if self.current_index < len(self.lines):
+        if self.current_index < len(self.lines_errors):
             line_errors = self.__getitem__(self.current_index)
             self.current_index += 1
             return line_errors
@@ -202,14 +322,16 @@ class PEPChecker:
         # check single lines errors
         for i, line in enumerate(self.lines):
             for error in PEPChecker.single_line_errors:
-                self.lines_errors[i] = error.check(line)
+                check = error.check(line)
+                if check:
+                    self.lines_errors[i].append(check)
 
         # looking for multiline errors
         for error in PEPChecker.multi_line_errors:
-            lines_with_err = error.check(self.lines)
-            for line in lines_with_err:
-                self.lines_errors[line].append(error())
-
+            list_with_err = error.check(self.lines)
+            for i, element in enumerate(list_with_err):
+                if element:
+                    self.lines_errors[i].append(element)
 
     @staticmethod
     def check_line(line, error):
